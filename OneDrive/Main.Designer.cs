e044541fe8fa684f68d06dc4ec9516ebb8e377eb
bdz4sem <bdz4sem@gmail.com>
2015-04-25 04:13:26 +0400

    encryption, signout

---
+++ OneDrive/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace OneDrive
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.SignIn = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.meImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.meImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SignIn
+            // 
+            this.SignIn.Location = new System.Drawing.Point(1023, 576);
+            this.SignIn.Name = "SignIn";
+            this.SignIn.Size = new System.Drawing.Size(75, 23);
+            this.SignIn.TabIndex = 0;
+            this.SignIn.Text = "Sign In";
+            this.SignIn.UseVisualStyleBackColor = true;
+            this.SignIn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(984, 95);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(29, 13);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "User";
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(0, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(103, 611);
+            this.splitter2.TabIndex = 4;
+            this.splitter2.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(367, 587);
+            this.treeView1.TabIndex = 6;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(482, 12);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(367, 587);
+            this.treeView2.TabIndex = 7;
+            // 
+            // meImage
+            // 
+            this.meImage.Image = global::OneDrive.Properties.Resources.noavatar;
+            this.meImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("meImage.InitialImage")));
+            this.meImage.Location = new System.Drawing.Point(987, 12);
+            this.meImage.Name = "meImage";
+            this.meImage.Size = new System.Drawing.Size(80, 80);
+            this.meImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.meImage.TabIndex = 1;
+            this.meImage.TabStop = false;
+            this.meImage.WaitOnLoad = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 611);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.meImage);
+            this.Controls.Add(this.SignIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Mephi One Drive Client";
+            ((System.ComponentModel.ISupportInitialize)(this.meImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SignIn;
+        private System.Windows.Forms.PictureBox meImage;
+        public System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+    }
+}
+
---
+++ OneDrive/Main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;    
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Live;
+
+
+namespace OneDrive
+{
+    public partial class Main : Form, IRefreshTokenHandler
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        
+        public AuthResult authResult;
+        string ClientID = "000000004814AD4C";
+        MyBrowser myBrowser;
+        LiveAuthClient liveAuthClient;
+        LiveConnectClient liveConnectClient;
+        private Microsoft.Live.RefreshTokenInfo refreshTokenInfo;
+
+        string[] Scopes = { "wl.signin", "wl.basic", "wl.photos", "wl.share", "wl.skydrive", "wl.skydrive_update", "wl.work_profile" };
+        LiveAuthClient AuthClient
+        {
+            get
+            {
+                if (this.liveAuthClient == null)
+                {
+                    this.AuthClient = new LiveAuthClient(ClientID, this);
+                }
+
+                return this.liveAuthClient;
+            }
+
+            set // удалена обработка ошибок
+            {
+                this.liveAuthClient = value;
+                this.liveConnectClient = null;
+            }
+        }
+        private void CleanupBrowser()
+        {
+            if (this.myBrowser != null)
+            {
+                this.myBrowser.Dispose();
+                this.myBrowser = null;
+            }
+        } 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string startUri = this.AuthClient.GetLoginUrl(Scopes);
+            string endUri = "https://login.live.com/oauth20_desktop.srf";
+            this.myBrowser = new MyBrowser(startUri, endUri, this.OnAuthCompleted);
+            this.myBrowser.Show();
+            this.myBrowser.MyBrowser_Load();
+        }
+        private async void OnAuthCompleted(AuthResult authResult)
+        {
+            this.CleanupBrowser();
+            if (authResult.AuthorizeCode != null)
+            {
+                try
+                {
+                    LiveConnectSession session = await this.AuthClient.ExchangeAuthCodeAsync(authResult.AuthorizeCode);
+                    this.liveConnectClient = new LiveConnectClient(session);
+                    LiveOperationResult meRes = await this.liveConnectClient.GetAsync("me");
+                    dynamic meData = meRes.Result;
+                    this.nameLabel.Text = meData.name;
+                    LiveDownloadOperationResult meImgResult = await this.liveConnectClient.DownloadAsync("me/picture");
+                    this.meImage.Image = Image.FromStream(meImgResult.Stream);
+
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Error", e.Message);
+                }
+            }
+        }
+        Task IRefreshTokenHandler.SaveRefreshTokenAsync(RefreshTokenInfo tokenInfo)// хз
+        {
+            // Note: 
+            // 1) In order to receive refresh token, wl.offline_access scope is needed.
+            // 2) Alternatively, we can persist the refresh token.
+            return Task.Factory.StartNew(() =>
+            {
+                this.refreshTokenInfo = tokenInfo;
+            });
+        }
+        Task<RefreshTokenInfo> IRefreshTokenHandler.RetrieveRefreshTokenAsync() // хз
+        {
+            return Task.Factory.StartNew<RefreshTokenInfo>(() =>
+            {
+                return this.refreshTokenInfo;
+            });
+        }
+    }
+}
